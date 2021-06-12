403615845325b7437ceb621dd043bf0095e347ba
svetlanaaseeva <antaseev@yandex.ru>
2021-06-12 14:24:25 +0300

    JM_1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,25 +1,15 @@
 package jm.task.core.jdbc.util;
 
-//import com.mysql.fabric.jdbc.FabricMySQLDriver;
-
 import java.sql.*;
 
 public class Util {
-//    private static Connection connect;
 
     private static final String URL = "jdbc:mysql://localhost:3306/mydb_jm_1.1.3";
-//    private static final String URL = "jdbc:mysql://localhost:3306/JM?autoReconnect=true&useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Fhvtqwscytds178";
 
-//    public Connection getConnect() {
-//        return connect;
-//    }
-
     public static Connection connection() {
         try {
-//            Driver driver = new FabricMySQLDriver();
-//            DriverManager.deregisterDriver(driver);
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
